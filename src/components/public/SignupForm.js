d677bb31655ba8f4465e6596c70ac9f60e8ede4f
Jesse Mauro <jesse@devworkslab.com>
2017-06-26 23:20:46 -0400

    finished signup form

--- src/components/public/SignupForm.js
+++ src/components/public/SignupForm.js
@@ -12,6 +12,22 @@ export const SignupForm = () => (
                 <input type="text" name="flast_name" placeholder="Enter Last Name..." />
             </div>
             <div className="clear"></div>
+            <div className="input-container full">
+                <label>Email</label>
+                <input type="text" name="email" placeholder="Enter Email..." />
+            </div>
+            <div className="input-container half">
+                <label>Password</label>
+                <input type="password" name="password" placeholder="Enter Password..." />
+            </div>
+            <div className="input-container half align-right">
+                <label>Password Confirmation</label>
+                <input type="password" name="password_confirmation" placeholder="Confirm Password..." />
+            </div>
+            <div className="clear"></div>
+            <div className="input-container full">
+                <input type="submit" value="Sign Up" />
+            </div>
         </form>
     </div>
 )
